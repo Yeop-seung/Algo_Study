4e228c31336e5db3fd853d97f5a4cad3e38c18fb
Yeop-seung <83536433+Yeop-seung@users.noreply.github.com>
2022-03-31 15:02:51 +0900

    [Bronze III] Title: 별 찍기 - 1, Time: 136 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO 자동 생성된 메소드 스텁
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a = Integer.parseInt(st.nextToken());
+		String answer = "";
+		for (int i = 0; i < a; i++) {
+			answer+="*";
+			System.out.println(answer);
+		}
+	}
+}
