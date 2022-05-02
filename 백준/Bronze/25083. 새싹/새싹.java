fe5a2d4d86742133556db5a68a42462a9df490ec
Yeop-seung <83536433+Yeop-seung@users.noreply.github.com>
2022-05-02 19:10:45 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		try {
+System.out.println("         ,r'\"7\n"
+		+ "r`-_   ,'  ,/\n"
+		+ " \\. \". L_r'\n"
+		+ "   `~\\/\n"
+		+ "      |\n"
+		+ "      |");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.toString());
+		}
+	}
+}
