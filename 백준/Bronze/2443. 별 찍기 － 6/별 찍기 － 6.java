9a0e9cc36a65a4c944f3708c58914b16349f14d9
Yeop-seung <83536433+Yeop-seung@users.noreply.github.com>
2022-07-28 00:08:00 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 132 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO 자동 생성된 메소드 스텁
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			for (int j = N-i; j < N ; j++) {
+				sb.append(" ");
+			}
+			for (int j = (i + 1) * 2; j <= N*2; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
