d0bb0a93f0e7a4b92259771d98a07e068d70fff0
Yeop-seung <83536433+Yeop-seung@users.noreply.github.com>
2022-08-04 23:23:28 +0900

    [Silver III] Title: N과 M (8), Time: 176 ms, Memory: 18436 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] numbers;
+	static StringBuilder sb = new StringBuilder();
+	static boolean[] isChecked;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		numbers = new int[M];
+		isChecked = new boolean[N + 1];
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		loop(N, M, arr, 0, 0);
+
+		br.close();
+		sb.setLength(sb.length() - 1);
+		System.out.println(sb);
+	}
+
+	private static void loop(int N, int M, int[] arr, int cnt, int start) {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(numbers[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i < N; i++) {
+//			if (isChecked[i] == true)
+//				continue;
+			numbers[cnt] = arr[i];
+			isChecked[i] = true;
+			loop(N, M, arr, cnt + 1, i);
+			isChecked[i] = false;
+		}
+	}
+}
