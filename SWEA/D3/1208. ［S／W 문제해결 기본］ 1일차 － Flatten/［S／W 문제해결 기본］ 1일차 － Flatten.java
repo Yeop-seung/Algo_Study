740e2fcf70de197c59bb46880f3a53cfabee4c97
곽승엽 <83536433+Yeop-seung@users.noreply.github.com>
2026-07-24 14:06:06 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 111 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,50 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-
+public static int flattening(int[][] box) {
+        for (int j = 100; j >= 1; j--) {
+            for (int k = 1; k <= 100; k++) {
+                if (box[j][k] == 1) {
+                    for (int l = 1; l <= 100; l++) {
+                        for (int v = 1; v <= 100; v++) {
+                            if (box[l][v] == 1) {
+                                if (j == l) return 0;
+                                box[j][k] = 0;
+                                box[j - 1][k] = 1;
+                                box[l][v] = 0;
+                                box[l + 1][v] = 1;
+                                return j - l;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
 	public static void main(String[] args) throws IOException {
-		// TODO 자동 생성된 메소드 스텁
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
-		for (int i = 1; i <= 10; i++) {
-			sb.append("#").append(i).append(" ");
-			int N = Integer.parseInt(br.readLine());
-			st = new StringTokenizer(br.readLine());
-			int[] arr = new int[100];
+        for (int testcase = 1; testcase <= 10; testcase++) {
+            int dump = Integer.parseInt(br.readLine().trim());
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-			for (int j = 0; j < 100; j++) {
-				arr[j] = Integer.parseInt(st.nextToken());
-			}
-			Arrays.sort(arr);
-			for (int j = 0; j < N; j++) {
-				int loopNum = 0;
-				arr[99] -= 1;
-				arr[0] += 1;
-				while (arr[loopNum] > arr[loopNum + 1] && loopNum < 100) {
-					int temp = arr[loopNum];
-					arr[loopNum] = arr[loopNum + 1];
-					arr[loopNum + 1] = temp;
-					loopNum++;
-				}
-				loopNum = 1;
-				while (arr[100 - loopNum] < arr[100 - (loopNum + 1)] && loopNum < 100) {
-					int temp = arr[100 - loopNum];
-					arr[100 - loopNum] = arr[100 - (loopNum + 1)];
-					arr[100 - (loopNum + 1)] = temp;
-					loopNum++;
-				}
-				loopNum = 1;
-			}
-			sb.append(arr[99] - arr[0]).append("\n");
-		}
-		br.close();
-		sb.setLength(sb.length() - 1);
-		System.out.println(sb);
+            int[][] box = new int[101][101];
+            int maxHeight = Integer.MIN_VALUE;
+            int minHeight = Integer.MAX_VALUE;
+            for (int i = 1; i <= 100; i++) {
+                int height = Integer.parseInt(st.nextToken());
+                if (maxHeight < height) maxHeight = height;
+                if (minHeight > height) minHeight = height;
+                box[height][i] = 1;
+            }
+            int ans = 0;
+            if (maxHeight == minHeight) ans = 0;
+            else {
+                while (dump >= 0) {
+                    ans = flattening(box);
+                    if (ans == 0) break;
+                    dump--;
+                }
+            }
+
+
+            sb.append("#").append(testcase).append(" ").append(ans).append("\n");
+
+        }
+        if (sb.length() != 0)
+            sb.setLength(sb.length() - 1);
+
+        System.out.println(sb);
+        
 	}
+
 }
