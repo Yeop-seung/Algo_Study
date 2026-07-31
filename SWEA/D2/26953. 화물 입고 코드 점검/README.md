4ad8405fbb95cdc8b2c96f7150c364579ce38f46
곽승엽 <83536433+Yeop-seung@users.noreply.github.com>
2026-07-31 15:00:11 +0900

    [D2] Title: 화물 입고 코드 점검, Time: 104 ms, Memory: 29,312 KB -BaekjoonHub

---
+++ SWEA/D2/26953. 화물 입고 코드 점검/README.md
@@ -0,0 +1,15 @@
+# [D2] 화물 입고 코드 점검 - 26953 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZ6wpK9aHfHHBIQj) 
+
+### 성능 요약
+
+메모리: 29,312 KB, 시간: 104 ms, 코드길이: 3,213 Bytes
+
+### 제출 일자
+
+2026-07-31 14:59
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/26953. 화물 입고 코드 점검/화물 입고 코드 점검.java
@@ -0,0 +1,82 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine().trim());
+
+        for (int testcase = 1; testcase <= T; testcase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String code = st.nextToken();
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+
+            int[] count = new int[26];
+            StringBuilder sb1 = new StringBuilder();
+            for (char check : code.toLowerCase().toCharArray()) {
+                if (sb1.indexOf(String.valueOf(check)) == -1) {
+                    sb1.append(check);
+                }
+            }
+            code = sb1.toString();
+            for (char check : code.toLowerCase().toCharArray()) {
+                for (char temp : str.toLowerCase().toCharArray()) {
+                    if (check == temp) {
+                        int index = check - 'a';
+                        count[index]++;
+                    }
+                }
+            }
+
+            Arrays.sort(count);
+            int ans = count[25];
+
+            sb.append("#").append(testcase).append(" ").append(ans).append("\n");
+        }
+
+        if (sb.length() != 0)
+            sb.setLength(sb.length() - 1);
+
+        System.out.println(sb);
+	}
+}
