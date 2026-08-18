24d4ecbfbb1bfa4e94c15149c7d4ee02de7c8f81
곽승엽 <83536433+Yeop-seung@users.noreply.github.com>
2026-08-18 15:09:06 +0900

    [D3] Title: 제로, Time: 252 ms, Memory: 68,592 KB -BaekjoonHub

---
+++ SWEA/D3/8931. 제로/제로.java
@@ -0,0 +1,77 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int TC = Integer.parseInt(br.readLine().trim());
+
+        for (int testcase = 1; testcase <= TC; testcase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int K = Integer.parseInt(st.nextToken());
+
+            Stack<Integer> stack = new Stack<>();
+
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int num = Integer.parseInt(st.nextToken());
+                if (num != 0) {
+                    stack.push(num);
+                } else {
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    }
+                }
+            }
+
+            int sum = 0;
+            
+            for (int n : stack) {
+                sum += n;
+            }
+            sb.append("#").append(testcase).append(" ").append(sum).append("\n");
+        }
+        if (sb.length() > 0)
+            sb.setLength(sb.length() - 1);
+
+        System.out.print(sb);
+	}
+}
